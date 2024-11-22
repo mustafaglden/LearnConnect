6f08d3a53acc5ca2d52839ba26bd498d0a0528ac
Mustafa <mustafaagulden@gmail.com>
2024-11-22 13:39:58 +0300

    Initial Commit

---
+++ LearnConnect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnConnect
+//
+//  Created by Mustafa on 22.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LearnConnectTests/LearnConnectTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearnConnectTests.swift
+//  LearnConnectTests
+//
+//  Created by Mustafa on 22.11.2024.
+//
+
+import Testing
+@testable import LearnConnect
+
+struct LearnConnectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
